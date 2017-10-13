0d26ef3ecc41e7afdda1c2ec3c3151857813ad47
jang <jang@siot.do>
2017-10-14 08:35:04 +0900

    initial commit

---
+++ src/iamport.payment.ts
@@ -0,0 +1,16 @@
+export class IamportPayment {
+
+    private success: boolean;
+    private status: string;
+    private response : object;
+
+    constructor(response: {success: boolean, error_code: string, error_msg: string, imp_uid: string, merchant_uid: string,
+                            pay_method: string, paid_amount: number, status: string, name: string, pg_provider: string, pg_tid: string,
+                            buyer_name: string, buyer_email: string, buyer_tel: string, buyer_addr: string, buyer_postcode: string,
+                            custom_data: any, paid_at: number, receipt_url: string}) {
+        this.success = response.success;
+        this.status = response.status;
+        this.response = response;
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { IamportPayment } from "./iamport.payment";
+import { Platform } from "ionic-angular";
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+@Injectable()
+export class IamportService {
+
+    constructor(private platform: Platform, private inAppBrowser: InAppBrowser) {
+
+    }
+
+    private static parseQuery(query: string) {
+        let obj = {},
+            arr = query.split('&');
+
+        for (let element of arr) {
+            const pair = element.split("=" );
+            obj[ decodeURIComponent(pair[0]) ] = decodeURIComponent(pair[1]);
+        }
+
+        return obj;
+    }
+
+    public payment(userCode: string, param): Promise<IamportPayment> {
+        const promise = new Promise((resolve, reject) => {
+            this.platform.ready().then(()=> {
+                const paymentUrl = 'iamport-checkout.html?user-code=' + userCode;
+                const redirectUrl = "http://localhost/iamport";
+                const browser = this.inAppBrowser.create(paymentUrl,'_blank');
+                let paymentProgress = false;
+
+                param.m_redirect_url = redirectUrl;
+
+                browser.on("loadstart" )
+                    .subscribe(
+                        (e) => {
+                            if ( e.url.startsWith(redirectUrl) ) {
+                                const query  = e.url.substring( redirectUrl.length+1 );
+                                const data = IamportService.parseQuery(query);
+
+                                resolve(data);
+                            }
+                        }
+                    );
+
+                browser.on("loadstop" )
+                    .subscribe(
+                        (e) => {
+                            if ( !paymentProgress && (e.url).indexOf(paymentUrl) > -1 ) {
+                                paymentProgress = true;
+
+                                const inlineCallback = `(rsp) => {
+                                                        if( rsp.success ) {
+                                                            location.href = '${redirectUrl}?imp_success=true&imp_uid='+rsp.imp_uid+'&merchant_uid='+rsp.merchant_uid;
+                                                        } else {
+                                                            location.href = '${redirectUrl}?imp_success=false&imp_uid='+rsp.imp_uid+'&merchant_uid='+rsp.merchant_uid+'&error_msg='+rsp.error_msg;
+                                                        }
+                                                   }`;
+                                const iamport_script = `IMP.request_pay(${JSON.stringify(param)}, ${inlineCallback})`;
+
+                                browser.executeScript({
+                                    code : iamport_script
+                                });
+                            }
+                        }
+                    );
+
+                browser.on("exit" )
+                    .subscribe(
+                        (e) => {
+                            reject("사용자가 결제를 취소하였습니다.");
+                        }
+                    );
+
+                browser.show();
+            });
+        });
+
+        return promise;
+    }
+
+}
