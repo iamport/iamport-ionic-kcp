1138efe95fce9c07e7cc78ba3c21b25f0b1d53e4
jang <jang@siot.do>
2018-09-19 11:57:01 +0900

    본인인증 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IamportPayment } from "./iamport.payment";
+import { IamportCertification } from "./iamport.certification";
 import { Platform } from "ionic-angular";
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 
@@ -26,11 +27,11 @@ export class IamportService {
             }
         }
 
-        return new IamportPayment(obj);
+        return obj;
     }
 
     public payment(userCode: string, param): Promise<IamportPayment> {
-        const promise = new Promise((resolve, reject) => {
+        const promise = new Promise<IamportPayment>((resolve, reject) => {
             this.platform.ready().then(()=> {
                 const paymentUrl = 'iamport-checkout.html?user-code=' + userCode;
                 const redirectUrl = "http://localhost/iamport";
@@ -46,7 +47,7 @@ export class IamportService {
                                 const query  = e.url.substring( redirectUrl.length+1 );
                                 const data = IamportService.parseQuery(query);
 
-                                resolve(data);
+                                resolve(new IamportPayment(data));
                                 browser.close();
                             }
                         }
@@ -80,6 +81,66 @@ export class IamportService {
                     .subscribe(
                         (e) => {
                             reject("사용자가 결제를 취소하였습니다.");
+                        }
+                    );
+
+                browser.show();
+            });
+        });
+
+        return promise;
+    }
+
+    public certification(userCode: string, param): Promise<IamportCertification> {
+        const promise = new Promise<IamportCertification>((resolve, reject) => {
+            this.platform.ready().then(() => {
+                const certificationUrl = 'iamport-checkout.html?user-code=' + userCode;
+                const redirectUrl = "http://localhost/iamport-certification";
+                const browser = this.inAppBrowser.create(certificationUrl, '_blank', 'location=no');
+
+                let certificationProgress = false;
+
+                browser.on("loadstart" )
+                    .subscribe(
+                        (e) => {
+                            if ( e.url.startsWith(redirectUrl) ) {
+                                const query  = e.url.substring( redirectUrl.length+1 );
+                                const data = IamportService.parseQuery(query);
+
+                                resolve(new IamportCertification(data));
+                                browser.close();
+                            }
+                        }
+                    );
+
+                browser.on("loadstop" )
+                    .subscribe(
+                        (e) => {
+                            if ( !certificationProgress && (e.url).indexOf(certificationUrl) > -1 ) {
+                                certificationProgress = true;
+
+                                const inlineCallback = `(rsp) => {
+                                                        if( rsp.success ) {
+                                                            location.href = '${redirectUrl}?imp_success=true&imp_uid='+rsp.imp_uid+'&merchant_uid='+rsp.merchant_uid;
+                                                        } else {
+                                                            location.href = '${redirectUrl}?imp_success=false&imp_uid='+rsp.imp_uid+'&merchant_uid='+rsp.merchant_uid;
+                                                        }
+                                                   }`;
+                                const iamport_script = `IMP.certification(${JSON.stringify(param)}, ${inlineCallback})`;
+
+                                browser.executeScript({
+                                    code : iamport_script
+                                });
+                            }
+                        }, (e) => {
+
+                        }
+                    );
+
+                browser.on("exit" )
+                    .subscribe(
+                        (e) => {
+                            reject("사용자가 본인인증을 취소하였습니다.");
                         }
                     );
 
